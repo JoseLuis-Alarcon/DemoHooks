c57db8851e4a04cf3d36d0b6a0da2c79ac61ef53
Jose Luis Alarcon Vasquez <joseluis22av@gmail.com>
2024-11-28 11:06:16 -0500

    Demostración de formato de código

---
+++ DemoHooks/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Prueba de formateo de código");
+int edad = 25;
+if (edad >= 18)
+   {
+    Console.WriteLine("Es mayor de edad");
+        }
+else
+                {
+    Console.WriteLine("Es menor de edad");
+                            }
